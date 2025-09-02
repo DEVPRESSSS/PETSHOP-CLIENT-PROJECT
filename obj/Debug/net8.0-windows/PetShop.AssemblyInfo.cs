5fd789f35349316c19bfde8ab72ef0aea8ffa18e
devpresss xmont <xmontemorjerald@gmail.com>
2025-09-02 22:06:26 +0800

    Added mdf file and idf

--- obj/Debug/net8.0-windows/PetShop.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/PetShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PetShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ffb6715f3b53d5fd2893346491c7cb39a257069")]
 [assembly: System.Reflection.AssemblyProductAttribute("PetShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PetShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
